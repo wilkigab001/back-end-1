d04a4abe770727970426db2405d871d9c6a4d281
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-06 17:04:39 -0600

    added axios.post

--- server/index.js
+++ server/index.js
@@ -9,11 +9,30 @@ app.use(express.json())
 app.use(cors())
 
 //code here
+let friends = ['john','gabe', 'will', 'mason','lane','adam']
 
 app.get("/api/users", (req, res) => {
-    let friends = ['john', 'gabe', 'will', 'mason','lane','adam']
     res.status(200).send(friends);
 })
+
+// app.delete("api/users", (req, res) => {
+//     let existingFriend = req.params.friends
+//     for( i = 0; i < friends.length; i++){
+//         if(users[i].username === existingFriend){
+//             friends.splice(i, 1)
+//             res.status(200).send('User Deleted')
+//             return
+//         }
+//     }
+//     res.status(400).send('User not found.')
+// })
+
+app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    friends.push(req.body.test)
+    res.status(200).send(friends)
+})
+
 
 app.get("/weather/:temperature", (req, res) => {
     const { temperature } = req.params;
